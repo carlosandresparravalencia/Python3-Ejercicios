60859ee62a92d1dc2c2eb69c463fb90adade1b8d
carlosandresparravalencia <85460029+carlosandresparravalencia@users.noreply.github.com>
2021-06-06 16:59:49 -0500

    Update numero_menor_lista.py

--- numero_menor_lista.py
+++ numero_menor_lista.py
@@ -2,7 +2,7 @@ numeros = [7, 4, 16, 26, 9, 19, 23, 12, 3, 10]
 # Primer elemento de la lista
 menor = numeros[0]
 for numero in numeros:
-    # Modificar el valor de una variable menor si se cumple
+    # Modificar el valor de la variable "menor" si se cumple
     if numero < menor:
         menor = numero
 print(menor)
